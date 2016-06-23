14aa2c753b9764a4c442c4d0440cc65745108c00
yohanacespedes <yohana_310188@hotmail.com>
2016-06-23 02:03:27 -0400

    modificando el database para conectar con amazon prueba en amazon 3

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,7 +5,7 @@ return array(
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	'class'=>'CDbConnection',
-	'connectionString' => 'mysql:host=ec2-52-37-191-183.us-west-2.compute.amazonaws.com/;dbname=prestamo3',
+	'connectionString' => 'mysql:host=ec2-52-37-191-183.us-west-2.compute.amazonaws.com;dbname=prestamo3',
 	'emulatePrepare' => true,
 	'username' => 'root',
 	'password' => 'Estocada.25',
